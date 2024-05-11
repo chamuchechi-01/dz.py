3a82e0d1abd5fe9959c19dce355a0d60aaf05e1e
unknown <ametovamir2013@gmail.com>
2024-05-11 20:13:48 +0500

    lol

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-git = ("alo")
-gia = ('bla')
-hah = ("lol")
+# a = {1,2,3,4,5,6,12,24}
+# a.remove(12)
+# print(a)
+
+
+# a = {1,'Apple','jhon','CS','Mango','grapes'}
+# a.update([2,3,4])
+# print(a)
+
+
+# a = {96,65,2,}
+# b = {'joseph',1,'pedro',59}
+# a.update(b)
+# print(a)
+
+# a = (input("write country name"))
+# b = []
+# print(f"you have " + a + " in your list")
+
+# a = ["russia","uzb","france"]
+# print(a)
+# b = input("enter country for delete")
+# if b in a:
+#     a.remove(b)
+#     print(f"{b}has been remowed")
+# print("Updated list of countries:")
+# print(a)
