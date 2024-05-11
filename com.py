efda8f50494ea51e8fbb19a8fcfebc300dd8cc99
unknown <ametovamir2013@gmail.com>
2024-05-11 20:09:28 +0500

    lol

---
+++ com.py
@@ -0,0 +1,60 @@
+# try:
+#     print("this is try")
+# except:
+#     print("This is exception")
+
+# def divide(x, y):
+#     try: 
+#         if not type(x) is int:
+#             raise TypeError("man give me integer number or string")
+#         result = x // y
+#         print("you are sigma :) ",result)
+#     except Exception as e:
+#         print("bruh you suck :( ",e)
+#     finally:
+#         print("lol you finsh")
+# divide(3, "ihyun")
+
+
+# lst = ('a','c','b')
+# for x in lst:
+#     print(x)
+
+
+# dct = {
+#     'a': 1,
+#     'b': 2
+# }
+
+
+# lst1 = ['c','d']
+# lst2 = [3,4]
+
+
+# a = {1,2,3,4,5,6,12,24}
+# a.remove(12)
+# print(a)
+
+
+# a = {1,'Apple','jhon','CS','Mango','grapes'}
+# a.update([2,3,4])
+# print(a)
+
+
+# a = {96,65,2,}
+# b = {'joseph',1,'pedro',59}
+# a.update(b)
+# print(a)
+
+# a = (input("write country name"))
+# b = []
+# print(f"you have " + a + " in your list")
+
+# a = ["russia","uzb","france"]
+# print(a)
+# b = input("enter country for delete")
+# if b in a:
+#     a.remove(b)
+#     print(f"{b}has been remowed")
+# print("Updated list of countries:")
+# print(a)
